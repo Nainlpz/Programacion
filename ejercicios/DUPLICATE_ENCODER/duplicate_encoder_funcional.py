e0f5ef9d3343cd474c1403238501cdcd6420441e
Nainlpz <nain.lopez.2003@gmail.com>
2024-11-21 12:44:39 +0100

    test(duplicate_encoder.py): Eliminar los print que usaba para comprobar

--- ejercicios/DUPLICATE_ENCODER/duplicate_encoder_funcional.py
+++ ejercicios/DUPLICATE_ENCODER/duplicate_encoder_funcional.py
@@ -2,10 +2,6 @@ def duplicate_encode(word):
 
     return ''.join([')' if word.lower().count(characters) > 1 else '(' for characters in word.lower()])
 
-print(duplicate_encode('din'))
-print(duplicate_encode('recede'))
-print(duplicate_encode('Success'))
-
 if __name__ == '__main__':
 
     assert duplicate_encode("din"),"((("
